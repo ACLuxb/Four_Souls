3b3b0843ac2507a7486c0affde08e1413d746bc9
ACLuxb <fhb241597@fh-vie.ac.at>
2025-01-26 21:45:38 +0100

    Game Over Screen plus Health bar added

--- Four Souls/Assets/Scripts/Player/Player.cs
+++ Four Souls/Assets/Scripts/Player/Player.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -6,15 +8,22 @@ public class Player : MonoBehaviour
 
     private bool immunityFrames = false;
 
+    public static event Action OnPlayerDeath;  //event for Player death as reference for the game over screen
+
+    public Image HealthBar;             //referencing the health bar image
+    public float healthamount = 100f;   //getting a fill amount of the health bar depending on current health in percent
+
     public void TakeDamage(int damage)
     {
         if (immunityFrames == false)
         {
             this.health -= damage;
+            HealthBar.fillAmount = healthamount / 100f; //telling the healthbar the fillamount
 
             if (health <= 0)
             {
                 Destroy(gameObject);
+                OnPlayerDeath!.Invoke(); //event starts of player is dead
             }
             immunityFrames = true;
             Invoke("EndImmunity", 0.5f);
